d7200f5775eb4da47c3349c578f404b6f3eec7b8
Emma van der Heide <erv5124@psu.edu>
2024-09-12 13:25:46 -0400

    Test

---
+++ data_analysis.R
@@ -0,0 +1,3 @@
+#This script contains the code for data analysis presented in "Carduus nutans and C. acanthoides, two invasive thistle species, strongly rebound from disturbance"
+#by Emma van der Heide, Joseph Keller, Elyse Johnson, and Katriona Shea
+
